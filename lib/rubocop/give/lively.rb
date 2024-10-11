e46c99c9ec7064593a6e01d0d1085f74c253d059
Christian Lowe <christglowe@gmail.com>
2024-10-11 16:08:43 -0500

    initial gem setup

---
+++ lib/rubocop/give/lively.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "lively/version"
+
+module Rubocop
+  module Give
+    module Lively
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubocop/give/lively/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Rubocop
+  module Give
+    module Lively
+      VERSION = "0.1.0"
+    end
+  end
+end
