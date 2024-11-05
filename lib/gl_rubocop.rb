8cf8922a1a5bacb60807c10d98be5f8ecf837730
Christian Lowe <christglowe@gmail.com>
2024-11-05 13:30:30 -0600

    updates configuration load directory path

--- lib/gl_rubocop.rb
+++ lib/gl_rubocop.rb
@@ -1,12 +1,12 @@
 require 'rubocop'
 
 module GLRubocop
-  # PROJECT_ROOT = File.expand_path('../', __dir__)
-  # CONFIG_DEFAULT = File.join(PROJECT_ROOT, '.rubocop.yml')
+  PROJECT_ROOT = File.expand_path('../', __dir__)
+  CONFIG_DEFAULT = File.join(PROJECT_ROOT, '.rubocop.yml')
 
-  # # Register your custom RuboCop config
-  # RuboCop::ConfigLoader.default_configuration = RuboCop::ConfigLoader.merge_with_default(
-  #   RuboCop::ConfigLoader.load_file(CONFIG_DEFAULT),
-  #   CONFIG_DEFAULT
-  # )
+  # Register your custom RuboCop config
+  RuboCop::ConfigLoader.default_configuration = RuboCop::ConfigLoader.merge_with_default(
+    RuboCop::ConfigLoader.load_file(CONFIG_DEFAULT),
+    CONFIG_DEFAULT
+  )
 end
--- spec/gl_rubocop/gl_rubocop_spec.rb
+++ spec/gl_rubocop/gl_rubocop_spec.rb
@@ -4,4 +4,6 @@ RSpec.describe GLRubocop do
   it 'has a version number' do
     expect(GLRubocop::VERSION).not_to be_nil
   end
+
+  # TODO: Add specs to ensure proper configuration is used
 end
