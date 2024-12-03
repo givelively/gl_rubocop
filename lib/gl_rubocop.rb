d9e959ee8d76bf6b3f74780bfe4f7fa50459f323
Christian Lowe <christglowe@gmail.com>
2024-12-03 10:24:24 -0600

    exports configuration as rubocop.myl so that file inclusions/exclusion can be defined in this repo

--- lib/gl_rubocop.rb
+++ lib/gl_rubocop.rb
@@ -2,7 +2,7 @@ require 'rubocop'
 
 module GLRubocop
   PROJECT_ROOT = File.expand_path('../', __dir__)
-  CONFIG_DEFAULT = File.join(PROJECT_ROOT, '.rubocop.yml')
+  CONFIG_DEFAULT = File.join(PROJECT_ROOT, 'rubocop.yml')
 
   # Register your custom RuboCop config
   RuboCop::ConfigLoader.default_configuration = RuboCop::ConfigLoader.merge_with_default(
