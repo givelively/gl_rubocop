dd25ae34de02ec0420b3cb9a5c165e0f8ca97f33
Traci <tracir@gmail.com>
2025-01-14 13:43:44 -0800

    cleanup

--- lib/gl_rubocop/gl_cops/unique_identifier.rb
+++ lib/gl_rubocop/gl_cops/unique_identifier.rb
@@ -3,18 +3,19 @@ module GLRubocop
     class UniqueIdentifier < RuboCop::Cop::Base
       MSG = 'View components must include a data-test-id attribute'
       EMPTY_MSG = 'data-test-id attribute must not be empty'
+      UNIQUE_IDENTIFIER = 'data-test-id'
 
       def on_send(node)
+        return unless %i[render template].include?(node.method_name)
         return unless File.exist?(processed_source.file_path)
         
         raw_content = File.read(processed_source.file_path)
-        return add_offense(node) unless raw_content.include?('data-test-id')
+        return add_offense(node) unless raw_content.include?(UNIQUE_IDENTIFIER)
 
-        line_number = raw_content.lines.find_index { |line| line.include?('data-test-id') }
+        line_number = raw_content.lines.find_index { |line| line.include?(UNIQUE_IDENTIFIER) }
         line = raw_content.lines[line_number]
-
-        test_id_value = line.match(/data-test-id:\s*["']([^"']*)["']/)[1]
         
+        test_id_value = line.match(/#{Regexp.quote(UNIQUE_IDENTIFIER)}:\s*["']([^"']*)["']/)[1]
         add_offense(node, message: EMPTY_MSG) if test_id_value.strip.empty?
       end
     end
