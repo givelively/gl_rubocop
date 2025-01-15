e6a5f9c8a2805aeeab82603d47c5c44b56961c69
Traci <tracir@gmail.com>
2025-01-15 08:47:21 -0800

    refactoring, move dependency, catch instance variables

--- lib/gl_rubocop/gl_cops/unique_identifier.rb
+++ lib/gl_rubocop/gl_cops/unique_identifier.rb
@@ -1,25 +1,58 @@
 module GLRubocop
   module GLCops
     class UniqueIdentifier < RuboCop::Cop::Base
+      # This cop ensures that view components include a data-test-id attribute.
+      #
+      # Good:
+      #   {data-test-id: "unique-id"}
+      #
+      # Bad:
+      #   {data: {testId: "unique-id"}}
+      #   {data: {test: {id: "unique-id"}}
+
       MSG = 'View components must include a data-test-id attribute'.freeze
       EMPTY_MSG = 'data-test-id attribute must not be empty'.freeze
       UNIQUE_IDENTIFIER = 'data-test-id'.freeze
 
-      # rubocop:disable Metrics/AbcSize
       def on_send(node)
-        return unless %i[render template].include?(node.method_name)
-        return unless File.exist?(processed_source.file_path)
+        return unless file_exists? && valid_method_name?(node)
 
-        raw_content = File.read(processed_source.file_path)
         return add_offense(node) unless raw_content.include?(UNIQUE_IDENTIFIER)
 
-        line_number = raw_content.lines.find_index { |line| line.include?(UNIQUE_IDENTIFIER) }
-        line = raw_content.lines[line_number]
-
-        test_id_value = line.match(/#{Regexp.quote(UNIQUE_IDENTIFIER)}:\s*["']([^"']*)["']/)[1]
         add_offense(node, message: EMPTY_MSG) if test_id_value.strip.empty?
       end
-      # rubocop:enable Metrics/AbcSize
+
+      private
+
+      def file_exists?
+        File.exist?(processed_source.file_path)
+      end
+
+      def identifiable_line
+        line_number = raw_content.lines.find_index { |line| line.include?(UNIQUE_IDENTIFIER) }
+        raw_content.lines[line_number]
+      end
+
+      def raw_content
+        @raw_content ||= File.read(processed_source.file_path)
+      end
+
+      def regex_for_indentifier_and_value
+        /#{Regexp.quote(UNIQUE_IDENTIFIER)}:\s*(["']([^"']*)["']|@\w+)/
+      end
+
+      def test_id_value
+        match = identifiable_line.match(regex_for_indentifier_and_value)
+
+        return '' unless match
+
+        value = match[1]
+        value.start_with?('"', "'") ? value[1..-2] : value
+      end
+
+      def valid_method_name?(node)
+        node.method_name == :render || node.method_name == :template
+      end
     end
   end
 end
