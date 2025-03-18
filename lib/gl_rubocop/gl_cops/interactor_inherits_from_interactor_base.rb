fe78fbcd5242934afce1e190d47a97eb1fae27cd
Seth Herr <seth.william.herr@gmail.com>
2025-03-18 10:14:58 -0700

    Add Layout/ClassStructure (#12)
    * Add Layout/ClassStructure
    
    * alphabetize default.yml
    
    * Fix spec
    
    * lint

--- lib/gl_rubocop/gl_cops/interactor_inherits_from_interactor_base.rb
+++ lib/gl_rubocop/gl_cops/interactor_inherits_from_interactor_base.rb
@@ -1,14 +1,14 @@
 module GLRubocop
   module GLCops
     class InteractorInheritsFromInteractorBase < RuboCop::Cop::Cop
+      MSG = 'Interactor should inherit from InteractorBase'.freeze
+
       def on_class(klass)
         return unless klass.instance_of?(RuboCop::AST::ClassNode)
         return unless klass.parent_class.nil?
 
         add_offense(klass)
       end
-
-      MSG = 'Interactor should inherit from InteractorBase'.freeze
     end
   end
 end
--- spec/gl_rubocop/gl_rubocop_spec.rb
+++ spec/gl_rubocop/gl_rubocop_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe GLRubocop do
   end
 
   # TODO: Add specs to ensure proper configuration is used
+
+  describe 'default.yml file' do
+    it 'keys are ordered alphabetically' do
+      default_rules = YAML.safe_load_file('default.yml')
+      target_rule_keys = (default_rules.keys - %w[require]).sort
+
+      # The rules should be ordered alphabetically, except for require (which comes first)
+      expect(default_rules.keys).to eq(%w[require] + target_rule_keys)
+    end
+  end
 end
