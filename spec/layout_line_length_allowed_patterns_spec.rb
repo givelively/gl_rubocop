3d612c163cfe837eebad052ee61276ed82418eef
Michael Gorman <michael@givelively.org>
2025-08-01 10:01:31 -0500

    specs for the configurations

---
+++ spec/layout_line_length_allowed_patterns_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rubocop/rspec/support'
+require 'gl_rubocop/gl_cops/unique_identifier'
+
+RSpec.describe RuboCop::Cop::Layout::LineLength, :rubocop do
+  include RuboCop::RSpec::ExpectOffense
+
+  describe 'AllowedPatterns' do
+    subject(:cop) { RuboCop::Cop::Layout::LineLength.new(config) }
+
+    let(:config) do
+      RuboCop::Config.new(
+        'Layout/LineLength' => {
+          'Max' => 100,
+          'AllowedPatterns' => [
+            '^ *#',
+            '^\s*[\'\"].*[\'\"]\s*$'
+          ]
+        }
+      )
+    end
+
+    it 'does not register offense for long comment lines' do
+      expect_no_offenses(<<~RUBY)
+        #{'#' * 120}
+      RUBY
+
+      expect_no_offenses(<<~RUBY)
+        #{'#'  +'a' * 120}
+      RUBY
+    end
+
+    it 'does not register offense for long string literal lines' do
+      expect_no_offenses(<<~RUBY)
+        "#{'a' * 120}"
+      RUBY
+      expect_no_offenses(<<~RUBY)
+        '#{'b' * 120}'
+      RUBY
+    end
+
+    it 'registers offense for other long lines' do
+      expect_offense(<<~RUBY)
+        x = "#{'c' * 101}"
+                                                                                                            ^^^^^^^ Line is too long. [107/100]
+      RUBY
+    end
+  end
+end
