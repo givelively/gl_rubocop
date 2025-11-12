2bf756ad2010a22be43c05eb6f271fc7b13eb134
Seth Herr <seth.william.herr@gmail.com>
2025-11-12 08:20:37 -0800

    v0.3.3 - Add ViewComponent initialize keyword args cop (#33)
    * Add ViewComponent initialize keyword args cop
    
    - Requires all ViewComponent initialize methods to use keyword arguments only
    - Applies to files in app/components/ with *_component.rb filenames
    - Includes comprehensive test coverage

---
+++ lib/gl_rubocop/gl_cops/view_component_initialize_keyword_args.rb
@@ -0,0 +1,30 @@
+# rubocop:disable I18n/RailsI18n/DecorateString
+module GLRubocop
+  module GLCops
+    # This cop ensures that ViewComponent initialize methods use keyword arguments only.
+    #
+    # Good:
+    #   def initialize(name:, age:)
+    #   def initialize(name:, age: 18)
+    #   def initialize(**options)
+    #
+    # Bad:
+    #   def initialize(name, age)
+    #   def initialize(name, age:)
+    class ViewComponentInitializeKeywordArgs < RuboCop::Cop::Cop
+      MSG = 'ViewComponent initialize methods must use keyword arguments only.'.freeze
+
+      def on_def(node)
+        return unless node.method_name == :initialize
+        return if node.arguments.empty?
+
+        has_positional_args = node.arguments.any? do |arg|
+          arg.arg_type? || arg.optarg_type? || arg.restarg_type?
+        end
+
+        add_offense(node, message: MSG) if has_positional_args
+      end
+    end
+  end
+end
+# rubocop:enable I18n/RailsI18n/DecorateString
