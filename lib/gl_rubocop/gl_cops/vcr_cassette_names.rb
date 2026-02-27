fb502892cd469d165eff1ea704836d212930e4bb
Seth Herr <seth.william.herr@gmail.com>
2026-02-27 12:17:42 -0800

    Add VcrCassetteNames cop to enforce named VCR cassettes
    - Detects VCR.use_cassette calls without string names
    - Detects RSpec metadata :vcr without cassette_name
    - Validates vcr hash metadata contains cassette_name key
    - Includes comprehensive specs with 11 test cases

--- lib/gl_rubocop/gl_cops/vcr_cassette_names.rb
+++ lib/gl_rubocop/gl_cops/vcr_cassette_names.rb
@@ -7,25 +7,73 @@ module GLRubocop
     # Good:
     #   VCR.use_cassette('cassette_name') { ... }
     #   VCR.use_cassette("cassette_name") { ... }
+    #   describe '.create', vcr: { cassette_name: :chariot_connect_create } do
     #
     # Bad:
     #   VCR.use_cassette { ... }
     #   VCR.use_cassette() { ... }
+    #   describe 'something', :vcr do
     class VcrCassetteNames < RuboCop::Cop::Cop
       MSG = 'VCR cassettes must have a name. Example: VCR.use_cassette("cassette_name") { ... }'
+      RSPEC_MSG = 'VCR cassettes must have a name. Example: describe "test", vcr: { cassette_name: :my_cassette } do'
+
+      RSPEC_METHODS = %i[describe context it specify example].freeze
 
       def_node_matcher :vcr_use_cassette?, <<~PATTERN
         (send (const nil? :VCR) :use_cassette ...)
       PATTERN
 
+      def_node_matcher :rspec_vcr_symbol?, <<~PATTERN
+        (sym :vcr)
+      PATTERN
+
+      def_node_matcher :rspec_vcr_hash?, <<~PATTERN
+        (hash (pair (sym :vcr) $_))
+      PATTERN
+
       def on_send(node)
+        check_vcr_use_cassette(node)
+        check_rspec_metadata(node)
+      end
+
+      private
+
+      def check_vcr_use_cassette(node)
         return unless vcr_use_cassette?(node)
         return if has_cassette_name?(node)
 
         add_offense(node, message: MSG)
       end
 
-      private
+      def check_rspec_metadata(node)
+        return unless RSPEC_METHODS.include?(node.method_name)
+
+        node.arguments.each do |arg|
+          if rspec_vcr_symbol?(arg)
+            add_offense(arg, message: RSPEC_MSG)
+          elsif arg.hash_type?
+            check_vcr_hash_metadata(arg)
+          end
+        end
+      end
+
+      def check_vcr_hash_metadata(hash_node)
+        hash_node.pairs.each do |pair|
+          next unless pair.key.sym_type? && pair.key.value == :vcr
+
+          value = pair.value
+          if value.hash_type?
+            # Check if the hash contains cassette_name key
+            has_cassette_name = value.pairs.any? do |inner_pair|
+              inner_pair.key.sym_type? && inner_pair.key.value == :cassette_name
+            end
+            add_offense(pair, message: RSPEC_MSG) unless has_cassette_name
+          else
+            # vcr: true or vcr: some_value without hash
+            add_offense(pair, message: RSPEC_MSG)
+          end
+        end
+      end
 
       def has_cassette_name?(node)
         # Check if the first argument exists and is a string
--- spec/gl_rubocop/gl_cops/vcr_cassette_names_spec.rb
+++ spec/gl_rubocop/gl_cops/vcr_cassette_names_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe GLRubocop::GLCops::VcrCassetteNames do
       'Example: VCR.use_cassette("cassette_name") { ... }'
   end
 
+  let(:expected_rspec_message) do
+    'GLCops/VcrCassetteNames: VCR cassettes must have a name. ' \
+      'Example: describe "test", vcr: { cassette_name: :my_cassette } do'
+  end
+
   it 'registers an offense when VCR.use_cassette has no name' do
     source = <<~RUBY
       VCR.use_cassette do
@@ -92,5 +97,80 @@ RSpec.describe GLRubocop::GLCops::VcrCassetteNames do
     report = commissioner.investigate(processed_source)
 
     expect(report.offenses.size).to eq(0)
+  end
+
+  context 'with RSpec metadata' do
+    it 'registers an offense when using :vcr symbol without cassette_name' do
+      source = <<~RUBY
+        RSpec.describe DistributeData::FormatAndSendData, :vcr, type: :command do
+          it 'does something' do
+          end
+        end
+      RUBY
+
+      processed_source = parse_source(source)
+      report = commissioner.investigate(processed_source)
+
+      expect(report.offenses.size).to eq(1)
+      expect(report.offenses.first.message).to eq(expected_rspec_message)
+    end
+
+    it 'registers an offense when using vcr hash without cassette_name' do
+      source = <<~RUBY
+        describe 'something', vcr: { match_requests_on: %i[method uri] } do
+          it 'does something' do
+          end
+        end
+      RUBY
+
+      processed_source = parse_source(source)
+      report = commissioner.investigate(processed_source)
+
+      expect(report.offenses.size).to eq(1)
+      expect(report.offenses.first.message).to eq(expected_rspec_message)
+    end
+
+    it 'does not register an offense when vcr hash has cassette_name' do
+      source = <<~RUBY
+        describe '.create', vcr: { cassette_name: :chariot_connect_create } do
+          it 'does something' do
+          end
+        end
+      RUBY
+
+      processed_source = parse_source(source)
+      report = commissioner.investigate(processed_source)
+
+      expect(report.offenses.size).to eq(0)
+    end
+
+    it 'does not register an offense when vcr hash has cassette_name with other options' do
+      source = <<~RUBY
+        context 'when the EIN exists', vcr: {
+          match_requests_on: %i[method uri_for_stripe],
+          cassette_name: :chariot_organization_search_found } do
+          it 'does something' do
+          end
+        end
+      RUBY
+
+      processed_source = parse_source(source)
+      report = commissioner.investigate(processed_source)
+
+      expect(report.offenses.size).to eq(0)
+    end
+
+    it 'works with it/specify/example blocks' do
+      source = <<~RUBY
+        it 'does something', :vcr do
+        end
+      RUBY
+
+      processed_source = parse_source(source)
+      report = commissioner.investigate(processed_source)
+
+      expect(report.offenses.size).to eq(1)
+      expect(report.offenses.first.message).to eq(expected_rspec_message)
+    end
   end
 end
