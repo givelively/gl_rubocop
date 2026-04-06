f13f6c67dfa2e87fa35bbb63e7081f7a1b0bb35a
Michael Gorman <michael@givelively.org>
2026-04-06 15:55:36 -0500

    lint fixes

--- spec/gl_rubocop/gl_cops/view_component_directory_structure_spec.rb
+++ spec/gl_rubocop/gl_cops/view_component_directory_structure_spec.rb
@@ -5,43 +5,43 @@ require 'rubocop/rspec/support'
 require 'gl_rubocop/gl_cops/view_component_directory_structure'
 
 RSpec.describe GLRubocop::GLCops::ViewComponentDirectoryStructure, :rubocop do
-	include RuboCop::RSpec::ExpectOffense
+  include RuboCop::RSpec::ExpectOffense
 
-	subject(:cop) { described_class.new(config) }
+  subject(:cop) { described_class.new(config) }
 
-	let(:config) { RuboCop::Config.new }
+  let(:config) { RuboCop::Config.new }
 
-	it 'does not register an offense for ApplicationViewComponent' do
-		expect_no_offenses(<<~RUBY)
-			class ApplicationViewComponent
-			end
-		RUBY
-	end
+  it 'does not register an offense for ApplicationViewComponent' do
+    expect_no_offenses(<<~RUBY)
+      class ApplicationViewComponent
+      end
+    RUBY
+  end
 
-	it 'does not register an offense when class belongs to an allowed base module' do
-		expect_no_offenses(<<~RUBY)
-			module Core
-				module Users
-					class Component < ApplicationViewComponent
-					end
-				end
-			end
-		RUBY
-	end
+  it 'does not register an offense when class belongs to an allowed base module' do
+    expect_no_offenses(<<~RUBY)
+      module Core
+        module Users
+          class Component < ApplicationViewComponent
+          end
+        end
+      end
+    RUBY
+  end
 
-	it 'registers an offense when class is under a disallowed base module' do
-		offenses = inspect_source(<<~RUBY)
-			module Billing
-				module Users
-					class Component < ApplicationViewComponent
-					end
-				end
-			end
-		RUBY
+  it 'registers an offense when class is under a disallowed base module' do
+    offenses = inspect_source(<<~RUBY)
+      module Billing
+        module Users
+          class Component < ApplicationViewComponent
+          end
+        end
+      end
+    RUBY
 
-		expect(offenses.size).to eq(1)
-		expect(offenses.first.message).to eq(
-			'GLCops/ViewComponentDirectoryStructure: ViewComponent must belong to an allowed base module: Core, Admin, NonprofitAdmin, Packs'
-		)
-	end
+    expect(offenses.size).to eq(1)
+    expect(offenses.first.message).to eq(
+      'GLCops/ViewComponentDirectoryStructure: ViewComponent must belong to an allowed base module: Core, Admin, NonprofitAdmin, Packs, Users'
+    )
+  end
 end
