ca7f27a64d313437399b865bb656c8175e8777c2
Michael Gorman <michael@givelively.org>
2026-05-06 16:48:43 -0500

    FIx view component naming cop (#39)

--- lib/gl_rubocop/gl_cops/view_component_class_naming.rb
+++ lib/gl_rubocop/gl_cops/view_component_class_naming.rb
@@ -1,24 +1,35 @@
 module GLRubocop
   module GLCops
-    # This cop checks that the class name is "Component" or "ApplicationViewComponent".
+    # This cop checks naming for classes inheriting from
+    # ApplicationViewComponent or ApplicationViewComponentPreview.
     #
     # Good:
-    #   class Component < ViewComponent::Base
+    #   class Component < ApplicationViewComponent
     #   end
     #
-    #   class ApplicationViewComponent < ViewComponent::Base
+    #   class ComponentPreview < ApplicationViewComponentPreview
     #   end
     #
     # Bad:
-    #   class UserCardComponent < ViewComponent::Base
+    #   class UserCardComponent < ApplicationViewComponent
+    #   end
+    #
+    #   class UserCardComponentPreview < ApplicationViewComponentPreview
     #   end
     class ViewComponentClassNaming < RuboCop::Cop::Base
       def on_class(node)
-        class_name = node.identifier.const_name
-        return true if class_name == 'Component'
-        return true if class_name == 'ApplicationViewComponent'
+        parent_class = node.parent_class&.const_name
+        if parent_class == 'ApplicationViewComponent'
+          return true if node.identifier.const_name == 'Component'
 
-        add_offense(node, message: 'ViewComponent class names must be "Component".')
+          add_offense(node, message: 'ViewComponent class names must be "Component".')
+        end
+        if parent_class == 'ApplicationViewComponentPreview'
+          return true if node.identifier.const_name == 'ComponentPreview'
+
+          add_offense(node, message: 'ViewComponentPreview class names must be "ComponentPreview".')
+        end
+        true
       end
     end
   end
